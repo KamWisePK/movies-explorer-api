3eef491a6ea0b885accaf2196872195694b41c04
Sergey Mudretskiy <mudreckii-sergei@yandex.ru>
2024-03-01 02:34:40 +1200

    mmn

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 module.exports = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { statusCode = 500, messageError } = err;
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    message: statusCode === 500 ? messageError : messageError,
   });
   next();
 };
