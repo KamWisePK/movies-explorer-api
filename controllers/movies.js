ddebebde1e581dc2efd775f6778537db1b110a22
Sergey Mudretskiy <mudreckii-sergei@yandex.ru>
2024-03-01 02:09:06 +1200

    aaa

--- controllers/movies.js
+++ controllers/movies.js
@@ -49,36 +49,36 @@ module.exports.getMovies = (req, res, next) => {
     .then((movies) => res.status(200).send(movies))
     .catch(next);
 };
-module.exports.deleteMovie = (req, res, next) => {
-  Movie.find({movieId:req.params._id})
-    .then((movie) => {
-      if (!movie) {
-        throw new NotFound('Запрашиваемый фильм не найдена');
-      } if (req.user._id !== movie.owner.toString()) {
-        throw new CurrentErr('Вы не можете удалять чужие фильмы');
-      }
-      movie.deleteOne();
-      res.send({ message: 'MOVIE_DELETE_MESSAGE '});
-    })
-    .catch(next);
-};
 // module.exports.deleteMovie = (req, res, next) => {
-//   const { movieId } = req.params;
-//   return Movie.findById(movieId)
+//   Movie.find({movieId:req.params._id})
 //     .then((movie) => {
 //       if (!movie) {
-//         throw new NotFound('Фильм не найден');
+//         throw new NotFound('Запрашиваемый фильм не найдена');
+//       } if (req.user._id !== movie.owner.toString()) {
+//         throw new CurrentErr('Вы не можете удалять чужие фильмы');
 //       }
-//       if (!movie.owner.equals(req.user._id)) {
-//         return next(
-//           new CurrentErr(
-//             'Вы не можете удалить фильм созданный другим пользователем',
-//           ),
-//         );
-//       }
-//       return movie
-//         .remove()
-//         .then(() => res.send({ message: 'Фильм успешно удален' }));
+//       movie.deleteOne();
+//       res.send({ message: 'MOVIE_DELETE_MESSAGE '});
 //     })
 //     .catch(next);
 // };
+module.exports.deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  return Movie.findById(movieId)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFound('Фильм не найден');
+      }
+      if (!movie.owner.equals(req.user._id)) {
+        return next(
+          new CurrentErr(
+            'Вы не можете удалить фильм созданный другим пользователем',
+          ),
+        );
+      }
+      return movie
+        .remove()
+        .then(() => res.send({ message: 'Фильм успешно удален' }));
+    })
+    .catch(next);
+};
